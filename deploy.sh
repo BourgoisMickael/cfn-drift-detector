9abb31ea24d4eac40b2bc923d20b5475d1884548
BourgoisMickael <mickael.bourgois@epitech.eu>
2022-11-24 00:49:39 +0100

    Setup typescript lambda

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source .env
+
+sam deploy \
+    --region eu-west-1 \
+    --template-file sam/template.yml \
+    --no-fail-on-empty-changeset \
+    --s3-bucket $S3_BUCKET \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --stack-name cfn-drift-detector \
+    --parameter-overrides "\
+        ParameterKey=DetectionSchedule,ParameterValue='$DETECTION_SCHEDULE' \
+        ParameterKey=DectionScheduleTimezone,ParameterValue=$DETECTION_SCHEDULE_TIMEZONE \
+        ParameterKey=Regions,ParameterValue=$REGIONS"
