2d26df8a869d30dc7e4999198c807bb232f01837
BourgoisMickael <mickael.bourgois@epitech.eu>
2022-12-18 23:09:38 +0100

    Put public policy on sererless application

--- publish.sh
+++ publish.sh
@@ -14,3 +14,16 @@ sam publish \
     --region $DEPLOY_REGION \
     --profile $DEPLOY_PROFILE \
     --template-file template-output.yml
+
+ACCOUNT_ID=$(
+    aws sts get-caller-identity \
+        --profile $DEPLOY_PROFILE \
+        --query Account \
+        --output text
+)
+
+aws serverlessrepo put-application-policy \
+    --region $DEPLOY_REGION \
+    --profile $DEPLOY_PROFILE \
+    --application-id arn:aws:serverlessrepo:$DEPLOY_REGION:$ACCOUNT_ID:applications/cfn-drift-detector \
+    --statements "Actions=Deploy,Principals=*,StatementId=PublicAccess"
